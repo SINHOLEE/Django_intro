92e90b067673836ae0478d63870d819e01c090b8
이신호 <dltlsgh5@naver.com>
2019-08-14 16:52:43 +0900

    leran django

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import random
+from datetime import datetime
 # Create your views here.
 
 
@@ -14,14 +15,77 @@ def introduce(request):
 
 
 # Template Variable example
-def dinner(request):
+def dinner(request, name):
     menu = ['강남 더막창스', '노랑통닭', '양자강']
 
     # 최대한 다른함수나 메소드, 어트리뷰트등 겹치지 않게 한다.
     pick = random.choice(menu)
     context = {
         'pick' : pick,
+        'name' : name,
     }
     # Django template로 context 전달
     return render(request, 'dinner.html', context)
     
+
+def image(request):
+    nums = random.randint(1,1010)
+    url = f'https://picsum.photos/id/{nums}/500/500'
+    context = {
+        'url' :url,
+    }
+    return render(request, 'image.html', context)
+
+
+def greeting(request, name):
+
+    context = {
+        'name' : name,
+    }
+    return render(request,'greeting.html', context)
+
+
+def times(request, num1, num2):
+    
+    result = num1 * num2
+  
+    def comma(num):
+        num = str(num)
+        num = num[::-1]
+        s = ''
+        for i in range(len(num)):
+            if i == 0:
+                s += num[i]
+
+            else:
+                if i % 3 == 0:
+                    s = s + ',' + num[i] 
+                else:
+                    s += num[i]
+        
+        s = s[::-1]
+        return s
+    context = {
+        'num1' : comma(num1),
+        'num2' : comma(num2),
+        'result' : comma(result),
+    }
+
+    return render(request, 'times.html', context)
+
+
+def template_language(request):
+    menu = ['짜장면', '탕수육', '양장피', '짬뽕']
+    my_santence = 'Life is short, you need python'
+    messages = ['apple', 'banana', 'cucumber', 'mango', 'bob']
+    datetimenow = datetime.now()
+    empty_list = []
+
+    context = {
+        'menu' : menu,
+        'my_santence' : my_santence,
+        'messages' : messages,
+        'datetimenow' : datetimenow,
+        'empty_list' : empty_list,
+    }
+    return render(request, 'template_language.html', context)
